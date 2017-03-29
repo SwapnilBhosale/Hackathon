2226c3a1082181a294082ee0016a2ed775185253
root <root@ip-172-31-6-230.us-west-2.compute.internal>
2017-03-29 20:58:23 +0000

    Add MySQL config

--- environment/api.py
+++ environment/api.py
@@ -1,24 +1,20 @@
 #import configparser  
 #from flask.ext.sqlalchemy import SQLAlchemy  
 from flask import Flask, jsonify, request
+from flaskext.mysql import MySQL
 #from mail import send 
 
-application = Flask(__name__)
+app= Flask(__name__)
 
-# Read config file
-#config = configparser.ConfigParser()  
-#config.read('rating_db.conf')
+mysql = MySQL()
 
-# MySQL configurations
-#application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + config.get('DB', 'user') + ':' + config.get('DB', 'password') + '@' + config.get('DB', 'host') + '/' + config.get('DB', 'db') 
-#print(' Config : '+application.config['SQLALCHEMY_DATABASE_URI'])
-#application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'master'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
-#mysql = SQLAlchemy()
-
-# map models
-
-@application.route("/test", methods=['POST'])
+@app.route("/test", methods=['POST'])
 def test():  
     content = (request.get_json(silent=True)["result"])
     print(content["action"])
@@ -35,7 +31,7 @@ def test():
         return jsonify(data)
           
     if content["action"] == "getLunchMenu":
-        return generateResponse("There's rice, chappatis, daal, mithai. Like it?")
+        return get_lunch_items()
     if content["action"] == "getSnacksMenu":
         return generateResponse("There's Poha. Like it?")
     if content["action"] == "makeACall":
@@ -51,6 +47,11 @@ def test():
 
 def booklunch(no):
     sendMail()
-
+def get_lunch_items():
+    cursor = mysql.connect().cursor()
+    cursor.execute("select items from lunch_items where items_date = CURDATE()")
+    data = cursor.fetchone()
+    return jsonify(data)
+    
 if __name__ == "__main__":  
-    application.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
