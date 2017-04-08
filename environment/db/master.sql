a38895b3a8905764d1a4a9112f95ccefc635d7e4
amit <amit.kumar@gslab.com>
2017-04-08 16:54:39 +0530

    sql update

--- environment/db/master.sql
+++ environment/db/master.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.11, for Win64 (x86_64)
 --
 -- Host: localhost    Database: master
 -- ------------------------------------------------------
--- Server version	5.7.17-0ubuntu0.16.04.1
+-- Server version	5.7.11-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -63,8 +63,37 @@ CREATE TABLE `lunch_items` (
 
 LOCK TABLES `lunch_items` WRITE;
 /*!40000 ALTER TABLE `lunch_items` DISABLE KEYS */;
-INSERT INTO `lunch_items` VALUES ('2017-03-29','chappatis,rice,brinjals,barfi','poha'),('2017-04-07','chappatis,rice,bhindi,onions','poha');
+INSERT INTO `lunch_items` VALUES ('2017-03-29','chappatis,rice,brinjals,barfi','poha'),('2017-04-07','chappatis,rice,bhindi,onions','poha'),('2017-04-07','chappatis,rice,brinjals,barfi','poha'),('2017-04-08','chappatis,rice,bhindi,onions','poha');
 /*!40000 ALTER TABLE `lunch_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` int(5) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` char(8) NOT NULL,
+  `employee_id` varchar(10) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `IDX_language_NAME` (`email`),
+  KEY `employee_id` (`employee_id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'swapnil.bhosale@gslab.com','botopia','gs-0913'),(2,'abhijeet.bhagat@gslab.com','botopia','gs-0834'),(3,'amit.kumar@gslab.com','botopia','gs-0973');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -76,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-08  5:54:14
+-- Dump completed on 2017-04-08 16:53:44
