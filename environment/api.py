4347fd4fca008b54d8366722af172ab1817d2ceb
amit <amit.kumar@gslab.com>
2017-04-08 11:29:28 +0530

    Session Added

--- environment/api.py
+++ environment/api.py
@@ -1,14 +1,15 @@
-#import configparser  
-#from flask.ext.sqlalchemy import SQLAlchemy  
-from flask import Flask, jsonify, request
-#from mail import send
-import  json
+# import configparser
+# from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request, session, Session,escape
+import flask
+# from mail import send
+import json
 from flask.ext.mysql import MySQL
-#from flask_mysql import MySQL
-#from mail import send 
 
-app= Flask(__name__)
+# from flask_mysql import MySQL
+# from mail import send
 
+app = Flask(__name__)
 mysql = MySQL()
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -17,33 +18,41 @@ app.config['MYSQL_DATABASE_DB'] = 'master'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-@app.route('/login', methods = ['POST'])
+
+@app.route('/login', methods=['POST'])
 def login():
     _email = request.form['email']
     _password = request.form['password']
     cursor = mysql.connect().cursor()
-    cursor.execute("select e.first_name,e.last_name from employees e,user u where u.email = '"+_email+"' AND u.password = '"+_password+"' AND e.employee_id=u.employee_id");
-    data = cursor.fetchone();
+    count = cursor.execute("select e.first_name,e.last_name from employees e,user u where u.email = '" + _email + "' AND u.password = '" + _password + "' AND e.employee_id=u.employee_id");
+    data = cursor.fetchone()
     returnData = None
-    if(data) :
+    if (count != 0):
+        session['email'] = request.form['email']
+        session['password'] =  request.form['password']
         returnData = {
-            "status" : True,
-            "data" : data
+            "status": True,
+            "data": data
         }
-    else :
+    else:
         returnData = {
-            "status" : False,
-            "errorMessage" : "Wrong username or password"
+            "status": False,
+            "errorMessage": "Wrong username or password"
         }
     return jsonify(returnData)
 
 
+# @app.route('/logout')
+# def logout():
+#     # remove the username from the session if it's there
+#     session.pop('email', None)
+
 @app.route("/test", methods=['POST'])
-def test():  
+def test():
     content = (request.get_json(silent=True)["result"])
     print(content["action"])
     d = []
-          
+
     if content["action"] == "getLunchMenu":
         return get_lunch_items()
     if content["action"] == "getSnacksMenu":
@@ -53,30 +62,37 @@ def test():
     if content["action"] == "bookingConfirmed":
         bookLunch(no=2)
         return generateResponse("A booking mail has been sent")
-    if content["action"] == "doWebRtcCall" :
-        #to do compare here name with the email ID and send email Id a data param
+    if content["action"] == "doWebRtcCall":
+        # to do compare here name with the email ID and send email Id a data param
         return get_mail_id(content)
     return "action not recognized"
 
-def generateResponse(msg,source,resData):
-  data = {
-      "speech" : msg,
-      "displayText" : msg
-      }
-  if(source):
-    data["source"] = source;
-    if(resData):
-      data["data"] = resData;
-  return jsonify(data)
+
+def generateResponse(msg, source, resData):
+    data = {
+        "speech": msg,
+        "displayText": msg
+    }
+    if (source):
+        data["source"] = source;
+        if (resData):
+            data["data"] = resData;
+    return jsonify(data)
+
 
 def booklunch(no):
     sendMail()
 
+
 def get_lunch_items():
-    cursor = mysql.connect().cursor()
-    cursor.execute("select items from lunch_items where items_date = CURDATE()")
-    data = cursor.fetchone()
-    return generateResponse("Theres " + data[0], "show", {"list": data})
+    if 'email' in session:
+        cursor = mysql.connect().cursor()
+        cursor.execute("select items from lunch_items where items_date = CURDATE()")
+        data = cursor.fetchone()
+        return generateResponse("Theres " + data[0], "show", {"list": data})
+    else:
+        return generateResponse("Not Logged in","",{})
+
 
 def get_snacks_items():
     cursor = mysql.connect().cursor()
@@ -84,21 +100,30 @@ def get_snacks_items():
     data = cursor.fetchone()
     return generateResponse("Theres " + data[0], "show", {"list": data})
 
+
 def request_facility():
     pass
+
 
 def get_mail_id(content):
     name = content["parameters"]["name"]
     cursor = mysql.connect().cursor()
-    num = cursor.execute('select email, first_name, last_name from employees where first_name like "%'+name+'%" or last_name like "%'+name+'%"')
+    num = cursor.execute(
+        'select email, first_name, last_name from employees where first_name like "%' + name + '%" or last_name like "%' + name + '%"')
     if (num > 2):
-      print("call ml")
-      return generateResponse("Did you mean... ?","call",{"to":"null", "name": "null"})
+        print("call ml")
+        return generateResponse("Did you mean... ?", "call", {"to": "null", "name": "null"})
     else:
-      data = cursor.fetchone()
-      return generateResponse("Call has been placed","call",{"to":data[0], "name": data[1] + ' ' + data[2]})
+        data = cursor.fetchone()
+        return generateResponse("Call has been placed", "call", {"to": data[0], "name": data[1] + ' ' + data[2]})
 
-if __name__ == "__main__":  
+
+if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    sess = Session()
+    app.debug = True
+    app.run()
     app.run(host='0.0.0.0')
 
 
